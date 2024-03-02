28a9a73609cc2d63645d49ffb8b937c27005b795
SeokjinDev <tjrwls1108011@gmail.com>
2024-03-03 02:53:02 +0900

    Refactor: Prefix Post

--- demo/src/main/java/com/blog/demo/board/PostController.java
+++ demo/src/main/java/com/blog/demo/board/PostController.java
@@ -6,31 +6,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-
+@RequestMapping("/post")
 @RequiredArgsConstructor
 @Controller
 public class PostController {
     private final PostService postService;
 
-    @PostMapping("/post/create")
+    @PostMapping("/create")
     public String createPost(@RequestParam("title") String title, @RequestParam("content") String content) {
         this.postService.create(title, content);
         return "redirect:/post/list";
     }
 
-    @GetMapping("/post/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Post> postList = this.postService.getPostList();
         model.addAttribute("postList", postList);
         return "post_list";
     }
     
-    @GetMapping("/post/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String list(Model model, @PathVariable("id") Integer id) {
         Post post = this.postService.getPost(id);
         model.addAttribute("post", post);
